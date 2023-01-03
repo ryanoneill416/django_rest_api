e2b7de6a6540b6320e8232de3e550e9a5be0633a
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-03 18:19:57 +0000

    Implement PostList view tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='test', password='pass')
+
+    def test_can_list_posts(self):
+        test = User.objects.get(username='test')
+        Post.objects.create(owner=test, title='test title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_can_create_post(self):
+        self.client.login(username='test', password='pass')
+        response = self.client.post('/posts/', {'title': 'test title'})
+        count = Post.objects.count()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(count, 1)
+
+    def test_unauthenticated_cannot_create_post(self):
+        response = self.client.post('/posts/',
+                                    {'title': 'unauthenticated post'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
