f31c39885094658de358007cf559814221c4fcbc
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-29 18:01:39 +0000

    adjust url

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -120,7 +120,7 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://moments-app-wt.herokuapp.com/"
+    "https://moments-app-wt.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'drf_api.urls'
